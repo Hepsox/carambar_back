164254d3b819b007b54048ec6a09b7d04f4d5d19
Hepsox <marie.delaire@outlook.com>
2025-06-22 14:22:15 +0200

    add CORS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
@@ -10,6 +11,7 @@ const {
   getRandomJoke,
 } = require("./controllers/jokeController.js");
 
+app.use(cors());
 app.use(express.json());
 
 syncDB();
